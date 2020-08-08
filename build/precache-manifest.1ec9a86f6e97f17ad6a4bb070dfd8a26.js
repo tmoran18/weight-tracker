0448c0b09b8a5bbce95395110f11f8fa6787d6a8
tmoran18 <moran.tim01@gmail.com>
2020-08-08 16:01:52 +1000

    add build for deployment

---
+++ build/precache-manifest.1ec9a86f6e97f17ad6a4bb070dfd8a26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef85a851f73da6b4ceaaed2c9e946164",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acbc166021eb22d81f24",
+    "url": "/static/css/main.2692705c.chunk.css"
+  },
+  {
+    "revision": "74354710d82c33d39442",
+    "url": "/static/js/2.8b5e053e.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.8b5e053e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acbc166021eb22d81f24",
+    "url": "/static/js/main.f1daa267.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
