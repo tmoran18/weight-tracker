4283df3e6f117aac90efa316bfd5afe274d945bd
tmoran18 <moran.tim01@gmail.com>
2020-09-20 08:23:39 +1000

    added point labels to Graph

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -1,64 +1,64 @@
-import React from "react";
-import { ResponsiveLine } from "@nivo/line";
+import React from 'react';
+import { ResponsiveLine } from '@nivo/line';
 
 const LineGraph = ({ graphData }) => {
-  return (
-    <div
-      style={{
-        height: "500px",
-        background: "",
-        margin: "2rem",
-      }}
-    >
-      <ResponsiveLine
-        data={graphData}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        xScale={{ type: "point" }}
-        yScale={{
-          type: "linear",
-          min: 75,
-          max: 98,
-          stacked: false,
-          reverse: false,
-        }}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-          orient: "bottom",
-          tickSize: 0,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "Date",
-          legendOffset: 36,
-          legendPosition: "middle",
-        }}
-        axisLeft={{
-          orient: "left",
-          tickSize: 0,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "Weight - KG",
-          legendOffset: -40,
-          legendPosition: "middle",
-        }}
-        enableGridX={false}
-        colors={(d) => d.color}
-        pointSize={7}
-        pointColor="#ffffff"
-        pointBorderWidth={2}
-        pointBorderColor={{ from: "serieColor" }}
-        pointLabel="y"
-        pointLabelYOffset={-12}
-        useMesh={false}
-        theme={{
-          fontSize: 16,
-          fontFamily: "Sans-serif",
-          textColor: "#00b2ff",
-        }}
-        legends={[]}
-      />
-    </div>
-  );
+	return (
+		<div
+			style={{
+				height: '500px',
+				background: '',
+				margin: '2rem',
+			}}>
+			<ResponsiveLine
+				data={graphData}
+				margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+				xScale={{ type: 'point' }}
+				yScale={{
+					type: 'linear',
+					min: 75,
+					max: 98,
+					stacked: false,
+					reverse: false,
+				}}
+				axisTop={null}
+				axisRight={null}
+				axisBottom={{
+					orient: 'bottom',
+					tickSize: 0,
+					tickPadding: 5,
+					tickRotation: 0,
+					legend: 'Date',
+					legendOffset: 36,
+					legendPosition: 'middle',
+				}}
+				axisLeft={{
+					orient: 'left',
+					tickSize: 0,
+					tickPadding: 5,
+					tickRotation: 0,
+					legend: 'Weight - KG',
+					legendOffset: -40,
+					legendPosition: 'middle',
+				}}
+				enableGridX={false}
+				colors={(d) => d.color}
+				pointSize={7}
+				pointColor='#ffffff'
+				pointBorderWidth={2}
+				pointBorderColor={{ from: 'serieColor' }}
+				pointLabel='y'
+				enablePointLabel={true}
+				pointLabelYOffset={-12}
+				useMesh={false}
+				theme={{
+					fontSize: 16,
+					fontFamily: 'Sans-serif',
+					textColor: '#00b2ff',
+				}}
+				legends={[]}
+			/>
+		</div>
+	);
 };
 
 export default LineGraph;
--- src/components/LineGraphRight.js
+++ src/components/LineGraphRight.js
@@ -1,62 +1,62 @@
-import React from "react";
-import { ResponsiveLine } from "@nivo/line";
+import React from 'react';
+import { ResponsiveLine } from '@nivo/line';
 
 const LineGraphRight = ({ graphData }) => {
-  return (
-    <div
-      className="pushups_graph"
-      style={{
-        height: "500px",
-        background: "",
-        maxHeight: "490px",
-        margin: "2rem",
-        boxShadow: "0 8px 18px rgba(0, 0, 0, 0.08)",
-        borderRadius: "10px",
-        paddingBottom: "100px",
-      }}
-    >
-      <ResponsiveLine
-        data={graphData}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        xScale={{ type: "point" }}
-        enableGridY={false}
-        yScale={{
-          type: "linear",
-          min: 0,
-          max: 110,
-          stacked: false,
-          reverse: false,
-        }}
-        axisTop={null}
-        axisRight={{
-          orient: "right",
-          tickSize: 0,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "Pushups",
-          legendOffset: 40,
-          legendPosition: "middle",
-        }}
-        axisBottom={null}
-        axisLeft={null}
-        enableGridX={false}
-        colors={(d) => d.color}
-        pointSize={7}
-        pointColor="#ffffff"
-        pointBorderWidth={2}
-        pointBorderColor={{ from: "serieColor" }}
-        pointLabel="y"
-        pointLabelYOffset={-12}
-        useMesh={false}
-        theme={{
-          fontSize: 16,
-          fontFamily: "Sans-serif",
-          textColor: "red",
-        }}
-        legends={[]}
-      />
-    </div>
-  );
+	return (
+		<div
+			className='pushups_graph'
+			style={{
+				height: '500px',
+				background: '',
+				maxHeight: '490px',
+				margin: '2rem',
+				boxShadow: '0 8px 18px rgba(0, 0, 0, 0.08)',
+				borderRadius: '10px',
+				paddingBottom: '100px',
+			}}>
+			<ResponsiveLine
+				data={graphData}
+				margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+				xScale={{ type: 'point' }}
+				enableGridY={false}
+				yScale={{
+					type: 'linear',
+					min: 0,
+					max: 110,
+					stacked: false,
+					reverse: false,
+				}}
+				axisTop={null}
+				axisRight={{
+					orient: 'right',
+					tickSize: 0,
+					tickPadding: 5,
+					tickRotation: 0,
+					legend: 'Pushups',
+					legendOffset: 40,
+					legendPosition: 'middle',
+				}}
+				axisBottom={null}
+				axisLeft={null}
+				enableGridX={false}
+				colors={(d) => d.color}
+				pointSize={7}
+				pointColor='#ffffff'
+				pointBorderWidth={2}
+				pointBorderColor={{ from: 'serieColor' }}
+				pointLabel='y'
+				enablePointLabel={true}
+				pointLabelYOffset={-12}
+				useMesh={false}
+				theme={{
+					fontSize: 16,
+					fontFamily: 'Sans-serif',
+					textColor: 'red',
+				}}
+				legends={[]}
+			/>
+		</div>
+	);
 };
 
 export default LineGraphRight;
